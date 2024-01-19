f2a7d7aff10bd2348529de4db4152f32cc138909
RHaseltineStrayer <137434583+RHaseltineStrayer@users.noreply.github.com>
2024-01-19 13:20:24 -0500

    Add files via upload

---
+++ Week1HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week.pkg1.hello.world;
+
+/**
+ *
+ * @author rhase
+ */
+public class Week1HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Output Welcome Message
+        System.out.println("Hello World from Netbeans!");
+    }
+    
+}
